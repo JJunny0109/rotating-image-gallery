e8edf169486664e2738014471324325128570ca3
fakeArmy <jjunny0109@gmail.com>
2023-08-29 18:27:45 +0900

    fix: Fix the rotation bug
    - Fix the bug of rotating axis
    - To handle it, must set display flex and justify content center!

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ let x = 0; // Degree of images
 
 // Adding Event Listeners
 prevBtn.addEventListener('click', () => {
-  x -= 45;
+  x += 45;
   console.log('prevBtn Pressed!', x);
-  imgArea.style.transform = `rotateX(-15deg) rotateY(${x}deg)`; // Rotate the images
+  imgArea.style.transform = `rotateX(-10deg) rotateY(${x}deg)`; // Rotate the images
 });
 
 nextBtn.addEventListener('click', () => {
-  x += 45;
+  x -= 45;
   console.log('nextBtn Pressed!', x);
-  imgArea.style.transform = `rotateX(-15deg) rotateY(${x}deg)`; // Rotate the images
+  imgArea.style.transform = `rotateX(-10deg) rotateY(${x}deg)`; // Rotate the images
 });
