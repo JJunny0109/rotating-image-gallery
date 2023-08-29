b9ac1d8fdad1e5e3d3358e344b485237137be263
fakeArmy <jjunny0109@gmail.com>
2023-08-29 17:42:54 +0900

    feat : Rotating imgs
    - Press btns to rotate imgs
    - Have to fix the axis Problem!

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rotating-image-gallery
 Day 10 - Press buttons to slide pictures using animations
+
+The imgs that i used are books read in Jangyu Book club
+
+- Using media query to adjust img sizes regardless of the devices users are using
+- 
--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
-const imgAreaEl = document.querySelector('.img-area');
+// Quering the DOM
+const imgArea = document.querySelector('.img-area');
 
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+// Initializing Variables
+let x = 0; // Degree of images
+
+// Adding Event Listeners
+prevBtn.addEventListener('click', () => {
+  x -= 45;
+  console.log('prevBtn Pressed!', x);
+  imgArea.style.transform = `rotateX(-15deg) rotateY(${x}deg)`; // Rotate the images
+});
+
+nextBtn.addEventListener('click', () => {
+  x += 45;
+  console.log('nextBtn Pressed!', x);
+  imgArea.style.transform = `rotateX(-15deg) rotateY(${x}deg)`; // Rotate the images
+});
